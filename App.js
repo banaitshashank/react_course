be1e49fd4272a4969906be9b68e5a0bd440bcd7b
Shashank Banait <shashank.banait@atmecs.com>
2024-07-17 16:41:41 +0530

    jsx

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const elem = <span>This is single element in span tag and outside of my Title function component </span>
+
+const title = (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+)
+
+// React Functional Component
+const HeadingComponent = () => ( 
+    <div id="container">
+        <title />
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />); 
